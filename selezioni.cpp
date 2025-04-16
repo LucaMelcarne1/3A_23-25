196210c9f357c6dcde0745948ea9a221bdaf97ec
LucaMelcarne1 <luca.melcarne@salveminialessano.edu.it>
2025-04-16 14:46:35 +0200

    selezioni.cpp
    Indentazione

--- selezioni.cpp
+++ selezioni.cpp
@@ -17,29 +17,29 @@ int main() {
 	cin >>giorno;
 	
 	switch (giorno) { //fa il controllo sull'input di giorno
-  	case 1:
-    	cout << "Lunedi'";
-    break;
-    case 2:
-    	cout << "Martedi'";
-    break;
-  	case 3:
-    	cout << "Mercoledi'";
-    break;
-  	case 4:
-    	cout << "Giovedi'";
-    break;
-  	case 5:
-    	cout << "Venerdi'";
-    break;
-  	case 6:
-    	cout << "Sabato";
-    break;
-  	case 7:
-    	cout << "Domenica";
-    break;
-    default: //caso in cui non si verifica nessuna delle condizioni precedenti.
-    	cout <<"Numero sbagliato!";
+	  	case 1:
+	    	cout << "Lunedi'";
+	    break;
+	    case 2:
+	    	cout << "Martedi'";
+	    break;
+	  	case 3:
+	    	cout << "Mercoledi'";
+	    break;
+	  	case 4:
+	    	cout << "Giovedi'";
+	    break;
+	  	case 5:
+	    	cout << "Venerdi'";
+	    break;
+	  	case 6:
+	    	cout << "Sabato";
+	    break;
+	  	case 7:
+	    	cout << "Domenica";
+	    break;
+	    default: //caso in cui non si verifica nessuna delle condizioni precedenti.
+	    	cout <<"Numero sbagliato!";
   }
 	
 
